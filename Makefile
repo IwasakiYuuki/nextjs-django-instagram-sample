1fbf1fca6c4b233a6892e6be8772e77c695ac43d
IwasakiYuuki <2445yuuki@gmail.com>
2023-08-06 23:20:07 +0900

    init

---
+++ Makefile
@@ -0,0 +1,11 @@
+all: up
+
+build:
+	(cd frontend && docker build . --network=host -t instagram-sample-frontend)
+	(cd backend && docker build . --network=host -t instagram-sample-backend)
+
+up: build
+	docker compose up -d
+
+down:
+	docker compose down
---
+++ frontend/lib/posts.js
@@ -0,0 +1,34 @@
+// 投稿一覧取得
+export async function getPostList() {
+  console.log(process.env.NEXT_PUBLIC_API_URL);
+  const apiRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/post_list/`, {
+    method: 'GET',
+  })
+  const posts = await apiRes.json()
+  return posts
+}
+
+// 投稿一覧ID取得
+export async function getPostIds() {
+  const apiRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/post_list/`, {
+    method: 'GET',
+  })
+  const posts = await apiRes.json()
+
+  return posts.map((post) => {
+    return {
+      params: {
+        id: String(post.id),
+      },
+    }
+  })
+}
+
+// 投稿詳細取得
+export async function getPostDetail(id) {
+  const apiRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/post_detail/${id}/`, {
+    method: 'GET',
+  })
+  const post = await apiRes.json()
+  return post
+}
